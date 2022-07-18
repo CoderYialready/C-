62c9e8d2104a4f9c22fec2fa9a3745039a167485
CoderYialready <2054507614@QQcom>
2022-07-18 20:00:40 +0800

    字符串

---
+++ 字符串/字符串/字符串.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	//char arr[] = "hello";
+	char arr1[] = "abc";
+	char arr2[] = { "a", };
+	///*printf("%s\n", arr1);
+	//printf("%s\n", arr2);*/
+	//int len = strlen("abc");
+	//printf("%d\n", len);
+	printf("%d\n", strlen(arr1));
+
+	printf("%d\n", strlen(arr2));
+	printf("%d\n", strlen(arr2));
+
+
+
+
+
+
+	return 0;
+
+}
+
