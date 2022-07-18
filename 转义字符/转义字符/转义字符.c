fdd56a283ff05c2e5ec5bea4d5e0ec2f6fdf3ad8
CoderYialready <2054507614@QQcom>
2022-07-18 19:58:42 +0800

    初识转义字符

---
+++ 转义字符/转义字符/转义字符.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	printf("(are you ok\?\?)\n");
+	printf("%c\n",'\'');
+	printf("\a\a\a");
+    printf("\130\n");
+	printf("\x13");
+
+
+
+	return 0;
+}
+
+
+
+
