10113766f381c0e79f90273cfe1052f2e6ec55d5
CoderYialready <2054507614@QQcom>
2022-07-28 18:52:36 +0800

    test_7_28

---
+++ test_7_28/test_7_28/test_7_28.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//test1 运用printf输出小飞机
+int main()
+{
+	printf("   ***\n");
+	printf("**********\n");
+	printf("**********\n");
+	printf(" **   **\n");
+	printf(" **   **\n");
+	//test2 判断一个数是否可以被5整除
+	int m = 0;
+	scanf("%d", &m);
+	if (m % 5 == 0)
+		printf("YES");
+	else
+		printf("NO");
+
+
+
+
+
+	return 0;
+}
+
+
+
