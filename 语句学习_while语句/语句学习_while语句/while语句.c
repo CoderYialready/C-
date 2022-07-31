6886d656f5d0c05398a868d14ad309267349ab8c
CoderYialready <2054507614@QQcom>
2022-07-31 22:18:54 +0800

    while语句

---
+++ 语句学习_while语句/语句学习_while语句/while语句.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int n = 1;
+//	while (n <= 10)
+//	{
+//		if (n == 5)
+//			break;
+//		printf("%d\n", n);
+//		n++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 1;
+//	while (n <= 10)
+//	{
+//		if (n == 5)
+//			continue;
+//		printf("%d\n", n);
+//		n++;
+//	}
+//	return 0;
+//}
+int main()
+{
+	int ch = getchar;
+
+	while ((ch = getchar()) != EOF)//ctrl+z ¶ÁÈ¡½áÊø
+	{
+		putchar(ch);
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
