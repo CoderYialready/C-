055ab6e2bef88fcb3b800cbe86b6dd3e4d642eb6
CoderYialready <2054507614@QQcom>
2022-08-02 09:38:10 +0800

    for语句

---
+++ for语句/for语句/for语句.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	/*int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (; j < 3; j++)
+		{
+			printf("hehe");
+		}
+	}*/
+	int k = 0;
+	int l = 0;
+	for (k = 0, l = 0; k = 0; k++)//k = 0是为假，不是赋值
+	{
+		printf("hehe");
+
+	}
+	return 0;
+}
+
