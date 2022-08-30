c2b28435b28b328eff4840a04994cba07cd09ea6
CoderYialready <2054507614@QQcom>
2022-08-30 20:42:03 +0800

    实现字符串逆序_条件

---
+++ 实现字符串逆序_条件/实现字符串逆序_条件/.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>    //条件:不使用C语言库函数
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+
+
+void reverse_string(char* str)
+{
+	int left = 0;
+	int right = my_strlen(str) - 1;
+	while (left < right)
+	{
+		char tem = str[left];
+		str[left] = str[right];
+		str[right] = tem;
+		left++;
+		right--;
+	}
+
+}
+int main()
+{
+	char arr[] = "abcdef";
+	reverse_string(arr);
+	printf("%s", arr);
+	return 0;
+}
