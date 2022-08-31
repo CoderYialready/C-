d75a72a89f6818797c242e5829f7db47927683ce
CoderYialready <2054507614@QQcom>
2022-08-31 20:37:21 +0800

    实现字符串逆序_逆序

---
+++ 实现字符串逆序_递归/实现字符串逆序_递归/.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+
+void reverse_string(char* str)
+{
+	char tem = *str;
+	int len = my_strlen(str);
+	*str = *(str + len - 1);
+	*(str + len - 1) = '\0';
+	if (*(str+1) >= 2)
+	{
+		reverse_string(str + 1);
+	}
+	*(str + len - 1) = tem;
+
+
+
+}
+int main()
+{
+	char arr[] = "abcdef";
+	reverse_string(arr);
+	printf("%s", arr);
+	return 0;
+}
