64e48616e5f2672d6d5bef43f0f6a3e428bc744f
CoderYialready <2054507614@QQcom>
2022-10-29 23:15:15 +0800

    浮点数存储

--- 6666/6666/.c
+++ 6666/6666/.c
@@ -5,9 +5,13 @@ int main()
 	double v1 = 15;
 	double v2 = 20;
 	double t1 = 600;
-	printf("%lf分钟后可以追上\n", ((15 * 600)/5.0)/60 );
+	printf("%lf分钟后可以追上\n", ((15 * 600) / 5.0) / 60);
 
 
+	int a = 2^2;
+	printf("%d\n", a);
 
 	return 0;
 }
+
+
