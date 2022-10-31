68e0e980092335ca055a84bffe75c212ad2e9742
CoderYialready <2054507614@QQcom>
2022-10-31 22:41:23 +0800

    指针数组

--- 6666/6666/.c
+++ 6666/6666/.c
@@ -2,14 +2,23 @@
 #include <stdio.h>
 int main()
 {
-	double v1 = 15;
-	double v2 = 20;
-	double t1 = 600;
-	printf("%lf分钟后可以追上\n", ((15 * 600) / 5.0) / 60);
+	//double v1 = 15;
+	///*double v2 = 20;
+	//double t1 = 600;
+	//printf("%lf分钟后可以追上\n", ((15 * 600) / 5.0) / 60);
 
 
-	int a = 2^2;
-	printf("%d\n", a);
+	//int a = 2^2;
+	//printf("%d\n", a);*/
+
+	//double a = 1.4000000;
+	//printf("%.3lf",a);
+
+	int a = 1, b;
+	scanf("%3d%3d",&a,&b);
+	printf("%d %d",a,b);
+
+
 
 	return 0;
 }
