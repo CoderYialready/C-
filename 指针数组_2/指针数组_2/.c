0e62cdd71bb9dffd8b367a3cd25e622f7ee2a4dd
CoderYialready <2054507614@QQcom>
2022-11-01 22:40:32 +0800

    数组指针_2

---
+++ 指针数组_2/指针数组_2/.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+void print(int(*arr)[5], int i, int j)
+{
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			printf("%d ",*(*(arr + i)+j));
+		}
+		printf("\n");
+	}
+}
+int main()
+{
+	int arr[3][5] = { {1,2,3,4,5},{2,3,4,5,6},{3,4,5,6,7} };
+	print(arr, 3, 5);
+
+
+	return 0;
+}
