0bedae5817720c2e171cab82d279a35745f03002
CoderYialready <2054507614@QQcom>
2023-01-21 23:33:56 +0800

    L1-012

---
+++ L1-012/L1-012.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int rec = 0;
+    int n = 0;
+    scanf("%d",&n);
+    printf("2^%d = %d",n,pow(2,n));
+    return 0;
+}
